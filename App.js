624f287e2c1b6c0c8dbe3c91f843575d698919b2
fadoua <fadelhannachi@gmail.com>
2022-03-15 14:01:55 +0100

    barre de recherche

---
+++ App.js
@@ -0,0 +1,20 @@
+/* import { StatusBar } from 'expo-status-bar'; */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Search from './Components/Search'
+
+export default class App extends React.Component {
+  render() {
+    return <Search />
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
