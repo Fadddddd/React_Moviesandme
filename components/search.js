502434ba4e0d3c20483acb9eb5c3829e833cd9d0
fadoua <fadelhannachi@gmail.com>
2022-03-15 16:09:56 +0100

    découverte des keys

--- components/search.js
+++ components/search.js
@@ -1,23 +1,46 @@
-// Components/Search.js
+//Components/Search.js
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { FlatList, View, TextInput, Button, StyleSheet } from 'react-native'
+import films from '../Helpers/filmData'
+import FilmItem from './FilmItem'
 
+const styles = StyleSheet.create({
+  main_container: {
+    marginTop: 20,
+    flex: 1,
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5,
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 class Search extends React.Component {
     render() {
         return (
-            <View style={{ marginTop: 20 }}>
-                <TextInput
-                style={{
-                marginLeft: 5,
-                marginRight: 5,
-                height: 50,
-                borderColor: '#000000',
-                borderWidth: 1,
-                paddingLeft: 5,
-                }}
-                placeholder='Titre du film' />
-                <Button title='Rechercher' onPress={() => { }} />
+            <View style={styles.main_container}>
+            <TextInput style={styles.textinput} placeholder="Titre du film" />
+                <Button title="Rechercher" onPress={() => { }} />
+                <View>
+            <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id.toString()}
+                   renderItem={({ item }) => <FilmItem film={item} />} />
+                   
+                    </View>
             </View>
         )
     }
