1c7815ba3076b89c6752780b8d0bd4613315cb17
fadoua <fadelhannachi@gmail.com>
2022-03-17 13:35:54 +0100

    usage de l'api, sécurité, .env

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,11 +1,10 @@
 // API/TMDBApi.js
 import axios from "axios";
-
-const API_TOKEN = "73508e442c76f63a37529bbf3347bb40";
+import { API_TOKEN } from "@env";
 
 const getFilmsFromApiWithSearchedText = async (text) => {
   const url =
-    "https://api.themoviedb.org/3/search/movie?api_key=73508e442c76f63a37529bbf3347bb40" +
+    "https://api.themoviedb.org/3/search/movie?api_key=API_TOKEN" +
     API_TOKEN +
     "&language=fr&query=" +
     text;
