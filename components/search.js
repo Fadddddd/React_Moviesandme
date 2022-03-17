94d7123760b242be8f33763c05d9faa188c3f019
fadoua <fadelhannachi@gmail.com>
2022-03-17 16:29:42 +0100

    modifications

--- components/search.js
+++ components/search.js
@@ -3,42 +3,63 @@ import { FlatList, View, TextInput, Button, StyleSheet } from "react-native";
 // import films from '../Helpers/filmsData'
 import FilmItem from "../Components/FilmItem";
 import getFilmsFromApiWithSearchedText from "../API/TMDBApi";
+import { ActivityIndicator } from 'react-native'
 
 class Search extends React.Component {
   render() {
-    console.log("RENDER");
+    console.log('RENDER')
     return (
       <View style={styles.main_container}>
         <TextInput
           placeholder="Titre du film"
           style={{ styles }}
           onChangeText={(text) => this._searchTextInputChanged(text)}
-        />{" "}
+        />{' '}
         <Button title="Rechercher" onPress={() => this._loadFilms()} />
         <FlatList
           data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => <FilmItem film={item} />}
         />
+        {this._displayLoading()}
       </View>
-    );
+    )
   }
   constructor(props) {
     super(props);
     this.searchedText = "";
-    this.state = { films: [] };
+    this.state = {
+      films: [],
+      isLoading: false, // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
+    };
   }
 
   _searchTextInputChanged(text) {
     this.searchedText = text; // Modification du texte recherché à chaque saisie de texte, sans passer par setState
   }
 
+  // Bien noter les deux setState
+  //   isLoading: True puis appel API puis lorsque l'API a répondu isLoading: False
   _loadFilms() {
+     if (this.state.isLoading) return
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true })
     getFilmsFromApiWithSearchedText(this.searchedText).then((data) => {
-      this.setState({ films: data.results });
+      this.setState({ films: data.results, isLoading:false });
     });
   }
-}
+  }
+  
+   _displayLoading() {
+        if (this.state.isLoading) {
+          return (
+            <View style={styles.loading_container}>
+              <ActivityIndicator size='large' />
+            {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+          </View>
+        )
+     }
+  }
 
 const styles = StyleSheet.create({
   main_container: {
