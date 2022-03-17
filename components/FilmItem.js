8153dfbf707d20697bcbfb5e3dfc8dce19633856
fadoua <fadelhannachi@gmail.com>
2022-03-17 11:04:23 +0100

    rendu avec image et description

--- components/FilmItem.js
+++ components/FilmItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
-import 'react-json-pretty/themes/acai.css'
+import 'react-json-pretty/themes/adventure_time.css'
 import JSONPretty from 'react-json-pretty'
 
 class FilmItem extends React.Component {
@@ -8,23 +8,63 @@ class FilmItem extends React.Component {
         const film = this.props.film
         console.log('test:' + this.props.film.title)
         return (
-            <View >
-                <JSONPretty data={film}></JSONPretty>
-                <Text>{film.title}</Text>
-                <Image style={styles.image}
-                    source={{
-                        uri: 'https://reactnative.dev/img/tiny_logo.png',
-                    }}
-                />
+            <View>
+        <View>
+          {/* <JSONPretty data={film}></JSONPretty> */}
+        </View>
+        <View style={styles.film_main_container}>
+          <Image
+              style={styles.image}
+              source={film.poster_path}
+          />
+          <View style={styles.film_description}>
+            <View style={styles.titreVote}>
+              <Text style={styles.titreFilm}>{film.title}</Text>
+              <Text>{film.vote_average}</Text>
             </View>
-        )
-    }
+            <Text style={styles.OverviewFilm}>{film.overview}</Text>
+            <Text style={styles.Release_date_film}>{film.release_date}</Text>
+          </View>
+        </View>
+      </View>
+    )
+  }
 }
 const styles = StyleSheet.create({
-    image: {
-        width: 20,
-        height: 20,
-    },
+  film_main_container: {
+    flexDirection: 'row',
+    margin: 20,
+  },
+  film_description: {
+    flexDirection:'column',
+    marginLeft: 20,
+  }, 
+  titreVote: {
+    flexDirection: 'row',
+    fontSize: 16,
+  },
+  titreFilm: {
+    marginRight: 50,
+    fontSize: 20,
+    minWidth: 300,
+    fontWeight: 'bold',
+  },
+  OverviewFilm: {
+    marginTop: 20,
+    marginBottom: 20,
+    maxWidth: 350,
+    minHeight: 100,
+  },
+  Release_date_film: {
+    fontSize: 16,
+    marginLeft: 250,
+  },
+  image: {
+        width: 120,
+        height: 180,
+        flexDirection:'column',
+    }
+
 })
 
 export default FilmItem
